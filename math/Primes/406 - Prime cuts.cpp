adb2c4d2b6f12ff84e9dcd9ba9dceb704dc2abab
Vasu Singh <85363195+vasucp1207@users.noreply.github.com>
2021-10-17 13:28:28 +0530

    Update 406 - Prime cuts.cpp

--- math/Primes/406 - Prime cuts.cpp
+++ math/Primes/406 - Prime cuts.cpp
@@ -1,42 +1,87 @@
-#include<algorithm>
-#include<bitset>
-#include<cstdio>
-#include<vector>
- 
+#include <bits/stdc++.h>
+
 using namespace std;
- 
-int N, C;
-bitset<10000010> bs;
-vector<int> primes;
- 
-void sieve(long long upper_bound) {
-    bs.set();
-    bs[0] = bs[1] = 0;
-    primes.push_back(1);
-    for(long long i = 2; i <= upper_bound + 1; i++) {
-        if(bs[i]) {
-            for(long long j = i * i; j <= upper_bound + 1; j += i)
-                bs[j] = 0;
-            primes.push_back((int) i);
-        }
-    }
+
+#define ll long long 
+#define ull unsigned long long
+
+vector<bool> flag(10000000);
+vector<ll> primes;
+
+void sievee(ll upperbound){
+	
+	for(ll i = 0; i < 10000000; i++)flag[i] = true;
+	flag[0] = flag[1] = false;
+	
+	for(ll i = 2; i < sqrt(upperbound); i++){
+		if(flag[i]){
+			for(ll j = i * i; j < upperbound; j += i)
+				flag[j] = false;
+			primes.push_back(i);
+		}
+	}
 }
- 
-int main() {
-    sieve(1000);
-    while(scanf("%d %d", &N, &C) == 2) {
-        int i = lower_bound(primes.begin(), primes.end(), N) - primes.begin();
-        while(i >= primes.size() || primes[i] > N) i--;
- 
-        i++; // increment index to represent size
-        int c = (i % 2)? C * 2 - 1 : C * 2; // get size of printed list
- 
-        int s = (i - c) > 0? (i - c) / 2 : 0; // calc start index
-        int e = (i - c) > 0? s + c : i; // calc end index
- 
-        printf("%d %d:", N, C);
-        for(int j = s; j < e; j++)
-            printf(" %d", primes[j]);
-        printf("\n\n");
-    }
+
+bool isPrime(ll n){
+	if(n < 10000000)return flag[n];
+	
+	for(ll i = 0; i < primes.size(); i++){
+		if(n % primes[i] == 0)return false;
+	}
+	
+	return true;
+}
+
+int main(){
+	
+	sievee(10000000);
+	
+	int n, c;
+	
+	while(cin >> n >> c){
+		
+		vector<int> list;
+		
+		printf("%d %d:", n, c);
+		
+		list.push_back(1);
+		for(int i = 2; i <= n; i++){
+			if(flag[i]){
+				list.push_back(i);
+			}
+		}
+		
+		int len = list.size();
+		if(len % 2){
+			int C = 2 * c - 1;
+			if(C >= len){
+				for(int i = 0; i < len; i++)
+					printf(" %d", list[i]);
+			}
+			else{
+				for(int i = (len - C) / 2; i < (len + C) / 2; i++)
+					printf(" %d", list[i]);
+			}
+		}
+		
+		else{
+			int C = 2 * c;
+			if(C >= len){
+				for(int i = 0; i < len; i++)
+					printf(" %d", list[i]);
+			}
+			else{
+				for(int i = (len - C) / 2;i< (len + C) / 2; i++)
+					printf(" %d", list[i]);
+			}
+		}
+		
+		printf("\n\n");
+	}
+	
+	
+	
+	
+	
+	return 0;
 }
