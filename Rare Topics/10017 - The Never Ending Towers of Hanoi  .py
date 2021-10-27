84a226cdd9ab7300c4635fa1e5bf7215dc9cb8e6
Vasu Singh <85363195+vasucp1207@users.noreply.github.com>
2021-10-27 16:59:39 +0530

    Update 10017 - The Never Ending Towers of Hanoi  .py

--- Rare Topics/10017 - The Never Ending Towers of Hanoi  .py
+++ Rare Topics/10017 - The Never Ending Towers of Hanoi  .py
@@ -2,9 +2,10 @@ def TOH(disc, src, dest, imd):
 	
 	if disc == 1:
 		print(1, " ", src, " ", dest)
+		return
 		
 	TOH(disc - 1, src, imd, dest)
-	print(disc, " ", src, " ", dest, " ", imd)
+	print(disc, " ", src, " ", dest)
 	TOH(disc - 1, imd, dest, src)		
 	
 n = 3
