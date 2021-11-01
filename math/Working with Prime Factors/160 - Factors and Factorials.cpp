c1dab391bcf5981fd7764edbf89273fb176de591
Vasu Singh <85363195+vasucp1207@users.noreply.github.com>
2021-11-01 22:11:29 +0530

    Update 160 - Factors and Factorials.cpp

--- math/Working with Prime Factors/160 - Factors and Factorials.cpp
+++ math/Working with Prime Factors/160 - Factors and Factorials.cpp
@@ -8,56 +8,63 @@ using namespace std;
 #define send {ios_base::sync_with_stdio(false);}
 #define help {cin.tie(NULL);}
 
-int p[] = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97};
-
 void solve(int n){
 	
-	vector<int> a(25, 0);
+	int primes[] = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97};
+	vector<int> fac(100, 0);
+	
 	int orignal = n;
 	
 	while(n != 1){
 		
-		int i = n;
-		int j = 0;
-		
-		while(i != 1){
+		int num = n;
+		int i = 0;
+		while(num != 1){
 			
-			int cnt = 0;
-			while(i % p[j] == 0){
-				
-				i /= p[j];
-				cnt++;
+			while(num % primes[i] == 0){
+					
+				fac[primes[i]]++;
+				num /= primes[i];
 			}
-			a[j] += cnt;
-			j++;
+			i++;
 		}
 		n--;
 	}
 	
-	int last;
-	int i;
-	for(i = 24; i >= 0; i--)
-		if(a[i] != 0)break;
-		
-	last = i;
-	
+	int flag = 0;
 	cout << setw(3) << orignal << "! =";
 	
-      for(int i = 0; i <= last; i++) {
-        if (i > 0 && i % 15 == 0) cout << endl << "      ";
-        cout << setw(3) << a[i];
-      }
-      cout << endl;
-   	
+	int i;
+	for(i = 0; i < fac.size(); i++) 
+		if(fac[i] != 0){
+			
+			flag++;
+			if(flag > 15){
+				cout << endl;
+				break;
+			}
+			cout << setw(3) << fac[i];
+		}
+		
+	if(flag > 15){
+//		cout << "yes";   
+		cout << setw(6) << " ";
+		for(int j = i; j < fac.size(); j++){
+			if(fac[j] != 0)
+				cout << setw(3) << 1;
+		}
+	} 
+			
+	cout << endl;
 }
 
 int main(){
 	
 	int n;
-	while(true){
+	
+	while(cin >> n){
 		
-		cin >> n;
-		if(n == 0)break;
+		if(n == 0) break;
 		
 		solve(n);
 	}
