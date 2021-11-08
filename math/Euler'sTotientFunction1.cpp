5c548130d013619bc0575ac2ffc5c3dd1eb0e331
Vasu Singh <85363195+vasucp1207@users.noreply.github.com>
2021-11-08 22:06:11 +0530

    Create Euler'sTotientFunction1.cpp

---
+++ math/Euler'sTotientFunction1.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int phi(int n){
+	
+	int res = n;
+	
+	for(int i = 2; i * i <= n; i++){
+		if(n % i == 0){
+			
+			res /= i;
+			res *= (i - 1);
+			
+			while(n % i == 0){
+				n /= i;
+			}
+		}
+	}
+	
+	if(n > 1) res /= n, res *= (n - 1);
+	
+	return res;
+}
+
+int main(){
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+		int n;
+		cin >> n;
+		
+		cout << phi(n) << endl;
+	}
+
+
+
+return 0;
+}
