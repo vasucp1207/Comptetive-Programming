d8f263fe11a33ff94836f4b57833a630b22ddee9
Vasu Singh <85363195+vasucp1207@users.noreply.github.com>
2021-11-09 23:33:57 +0530

    Create 10179	Irreducable Basic Fractions.cpp

---
+++ math/Function involving Prime Factors/10179	Irreducable Basic Fractions.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long 
+#define fi first
+#define se second
+#define send {ios_base::sync_with_stdio(false);}
+#define help {cin.tie(NULL);}
+
+int main(){
+	
+	int n;
+	while(cin >> n){
+		
+		if(n == 0) break;
+		
+		ll res = n;
+		for(int i = 2; i * i <= n; i++){
+			
+			if(n % i == 0){
+				res *= (i - 1);
+				res /= i;
+				
+				while(n % i == 0){
+					n /= i;
+				}
+			}
+		}
+		if(n != 1) res *= (n - 1), res /= n;
+		
+		cout << res << endl;
+	}
+		
+
+return 0;
+}
