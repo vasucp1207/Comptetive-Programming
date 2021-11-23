00c1b65b577476f8a7d4b3d8d64d6919c1156a73
Vasu Singh <85363195+vasucp1207@users.noreply.github.com>
2021-11-23 21:38:39 +0530

    Update 1004.cpp

--- POJ/01-Practice for Simple Computing/1004.cpp
+++ POJ/01-Practice for Simple Computing/1004.cpp
@@ -1,1 +1,19 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main(){
+
+	double ans = 0.0;
+	double x;
+	for(int i = 0; i < 12; i++){
+		
+		cin >> x;
+		ans += x;
+	}
+	cout << "$" << ans / 12.0;
+
+
+
+return 0;
+}
