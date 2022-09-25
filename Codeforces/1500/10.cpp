c3640fe3329d9ac5a1734b44bb3a02bbe832cb54
Vasu Singh <vasucp1207@gmail.com>
2022-09-25 23:34:12 +0530

    Create 10.cpp

---
+++ Codeforces/1500/10.cpp
@@ -0,0 +1,74 @@
+/* Divan and bitwise operations: https://codeforces.com/problemset/problem/1614/C */
+
+#include<iostream>
+#include<iomanip>
+#include<algorithm>
+#include<vector>
+#include<utility>
+#include<set>
+#include<unordered_set>
+#include<list>
+#include<iterator>
+#include<deque>
+#include<queue>
+#include<stack>
+#include<set>
+#include<bitset>
+#include<random>
+#include<map>
+#include<unordered_map>
+#include<stdio.h>
+#include<complex>
+#include<math.h>
+#include<cstring>
+#include<chrono>
+#include<string>
+
+#define lb lower_bound
+#define ub upper_bound
+#define ll long long
+#define pb push_back
+#define ppb pop_back
+#define fi first
+#define se second
+#define ln "\n"
+#define all(x) (x).begin(), (x).end()
+const int mod = 1e9 + 7;
+
+ll pwr(ll a, ll b) {a %= mod; ll res = 1; while (b > 0) {if (b & 1) res = res * a % mod; a = a * a % mod; b >>= 1;} return res;}
+ll mod_mul(ll a, ll b) {a = a % mod; b = b % mod; return (((a * b) % mod) + mod) % mod;}
+
+using namespace std;
+
+int main(){
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	#ifndef ONLINE_JUDGE
+    freopen("input.txt" , "r" , stdin);
+    freopen("output.txt", "w", stdout);
+	#endif
+
+	int t;
+	cin >> t;
+
+	while(t--){
+		ll n, m;
+		cin >> n >> m;
+		ll ans = 0;
+		ll orr = 0;
+		for(int i = 0; i < m; i++){
+			ll l, r, x;
+			cin >> l >> r >> x;
+			orr |= x;
+		}
+
+		ans = mod_mul(pwr(2, n - 1), orr);
+		cout << ans << endl;
+	}
+	
+	
+
+    return 0;
+}
