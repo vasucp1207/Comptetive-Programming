eca24e5ca31392139214990f1de4a54d156eaec0
Vasu Singh <vasucp1207@gmail.com>
2022-12-04 22:06:17 +0530

    Create 04.cpp

---
+++ 1000-1500/04.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<iomanip>
+#include<algorithm>
+#include<vector>
+#include<utility>
+#include<set>
+#include<unordered_set>
+#include<list>
+#include<iterator>
+#include<deque>
+#include<queue>
+#include<stack>
+#include<set>
+#include<bitset>
+#include<random>
+#include<map>
+#include<unordered_map>
+#include<chrono>
+#include<string>
+
+#define lb lower_bound
+#define ub upper_bound
+#define ll long long 
+#define pb push_back
+#define ppb pop_back
+#define fi first
+#define se second
+#define ln "\n"
+#define all(x) (x).begin(), (x).end()
+
+const ll mod = 1e9 + 7;
+const ll mod1 = 998244353;
+
+ll pwr(ll a, ll b) {a %= mod; ll res = 1; while (b > 0) {if (b & 1) res = res * a % mod; a = a * a % mod; b >>= 1;} return res;}
+ll mod_mul(ll a, ll b) {a = a % mod; b = b % mod; return (((a * b) % mod) + mod) % mod;}
+ll gcd(ll a, ll b) { if (b == 0) return a; return gcd(b, a % b);}
+
+using namespace std;
+
+int main(){
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	ll t; 
+	cin >> t; 
+	while(t--) {
+		ll n;
+		cin >> n;
+		ll a = 1;
+		ll g = 1e9 + 5;
+		for(ll i = 2; i * i <= n; i++) {
+			if(n % i == 0) {
+				ll p = n / i;
+				if(((p * (n - p)) / gcd(p, (n - p))) < g) {
+					a = n / i;
+					g = ((a * (n - a)) / gcd(a, (n - a)));
+				}
+			}
+		}
+		cout << a << " " << n - a << endl;
+	}
+
+    return 0;
+}
